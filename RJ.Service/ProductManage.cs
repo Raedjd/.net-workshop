71608c196b86477d35d6bf913b2434e373eb4e7a
Your Name <you@example.com>
2022-02-24 14:24:45 +0100

    week5/session1

--- RJ.Service/ProductManage.cs
+++ RJ.Service/ProductManage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using RJ.Domaine;
+using System.Linq;
 namespace RJ.Service
 {
     public delegate IList<Product> FindProduct(string r);
@@ -18,6 +19,71 @@ namespace RJ.Service
         {
             this.myProducts = myProducts;
         }
+
+        //////////////////////////////semaine5-seance1///////////////////////////////////////
+        
+        public IEnumerable<Product> Get5Chemical(double price)
+        {
+            var query = from prod in myProducts
+                        where prod is Chemical
+                        select prod;
+            return query.Skip(5);
+        }
+
+        public IEnumerable<Product> GetProductPrice(double price)
+        {
+            var query = from prod in myProducts
+                        where prod.Price>price
+                        select prod;
+            return query.Skip(2);
+        }
+
+        public double GetAveragePrice()
+        {
+            var query = from prod in myProducts
+                        select prod.Price;
+            return query.Average();
+
+        }
+
+        public Product GetMaxPrice()
+        {
+            var query = from prod in myProducts
+                        select prod;
+            return query.Max();
+
+        }
+
+        public int GetCountProduct(string city)
+        {
+            var query = from prod in myProducts
+                        where prod is Chemical && ((Chemical)prod).City.Contains(city)
+
+                        select prod;
+            return query.Count();
+        }
+
+        public List<Product> GetChemicalCity()
+        {
+            var query = from prod in myProducts
+                        where prod is Chemical orderby ((Chemical)prod).City
+                        select prod;
+            return query.ToList();
+        }
+        public void GetChemicalGroupByCity()
+        {
+            var query = from prod in myProducts
+                        where prod is Chemical
+                        orderby ((Chemical)prod).City
+                        group prod by ((Chemical)prod).City;
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Key);
+            }
+           
+        }
+
 
     }
 }
---
+++ RJ.Service/ProviderManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RJ.Domaine;
+using System.Linq;
+namespace RJ.Service
+{
+    class ProviderManager
+    {   //////////////////////////////semaine5-seance1///////////////////////////////////////
+        List<Provider> providers;
+        public ProviderManager(List<Provider> providers)
+        {
+            this.providers = providers;
+        }
+        public List<Provider> GetProviderByName(string name)
+        {
+            var query = from prov in providers
+                        where prov.UserName.Contains(name)
+                        select prov;
+            return query.ToList();
+        }
+
+        public Provider GetFirstProviderByName(string name)
+        {
+            var query = from prov in providers
+                        where prov.UserName.Contains(name)
+                        select prov;
+            return query.First();
+        }
+
+        public Provider GetProviderById(int id)
+        {
+            var query = from prov in providers
+                        where prov.Id==id
+                        select prov;
+            return query.Single();
+
+        }
+    }
+}
